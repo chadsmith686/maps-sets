79c060344c46deca82e6d93e01db8b05a6e9c382
Chad Smith <chadsmithmusic@icloud.com>
2021-03-12 13:31:54 -0800

    first commit

---
+++ notes.js
@@ -0,0 +1,33 @@
+const myMap = new Map(); //creates new map
+
+// to add a key value pair, use Set. (key, value)
+myMap.set(7, 'seven')
+myMap.set('7', 'seven string')
+myMap.set([], 'empty array')
+
+myMap.get(7);
+
+const add = (x, y) => x + y;
+const mult = (x, y) => x * y;
+
+const funcCalls = new Map();
+function.set(add, 1)
+function.set(mult, 0)
+
+const bandData = [
+    [3, '3 doors down'],
+    ['three', 'three dog night'],
+    ['nine', 'nine inch nails'],
+];
+
+// SETS:
+
+['nofilter', 'justsaying', 'winning', 'yolo']
+
+const bannedHashTags = new Set();
+
+bannedHashTags.add('bestlife').add('selfie');
+bannedHashTags.has('tbt') //false
+bannedHashTags.has('yolo') //true
+bannedHashTags.delete
+bannedHashTags.clear() //deletes everything
